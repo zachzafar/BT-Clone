da8e813bffeb2f27cf6223220b114830373fe847
Zavid <zavidegbue@gmail.com>
2021-07-21 21:45:32 +0100

    Fix async image fetching

--- src/LocalNews.js
+++ src/LocalNews.js
@@ -1,53 +1,65 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import { Link } from "react-router-dom";
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 function LocalNews() {
-  const [lists, setList] = useState([]);
+  const [list, setList] = useState([]);
 
   useEffect(() => {
-    loadPosts()
+    loadPosts();
   }, []);
 
   const loadPosts = async () => {
-    let list = [];
     try {
-      const posts = await axios.get("posts/");
-      const data = posts.data;
-      console.log(data);
-      data.map(async (post) => {
-        let featuredMedia = await axios.get(`media/${post.featured_media}`);
-        list.push({
+      const { data: posts } = await axios.get('posts/');
+      const media = [];
+
+      const promises = posts.map((post) => {
+        media.push({
           id: post.id,
           title: post.title.rendered,
           description: post.excerpt.rendered,
-          image: featuredMedia.data.guid.rendered,
         });
+        return axios.get(`media/${post.featured_media}`);
       });
 
-      setList(list);
+      Promise.all(promises)
+        .then((values) => {
+          values.forEach((featuredMedia, i) => {
+            media[i] = {
+              ...media[i],
+              image: featuredMedia.data.guid.rendered,
+            };
+          });
+        })
+        .finally(() => {
+          setList(media);
+        });
     } catch (err) {
-     console.log(err);
+      console.log(err);
     }
   };
-  console.log(lists);
+
   return (
     <div>
-      {lists.map((list) => {
-          return (<div key={list.id}>
-          <div className="article item" >
-            <img src={list.image} />
-            <div className="Title">
-              <Link to={`article/${list.id}`}><h5>{list.title}</h5></Link>
-            </div>
-            <div className="Title">
-              <h5>{list.description}</h5>
+      {list.map((item, key) => {
+        console.log(item);
+        return (
+          <div key={key}>
+            <div>
+              <img src={item.image} alt={'s'} />
+              <div className='Title'>
+                <Link to={`article/${item.id}`}>
+                  <h5>{item.title}</h5>
+                </Link>
+              </div>
+              <div className='Title'>
+                <h5>{item.description}</h5>
+              </div>
             </div>
           </div>
-        </div>)
-          }
-      )}
+        );
+      })}
     </div>
   );
 }
